0bb882ad0a22308eb5afc299209223bdb3e59bf7
Alan Antonio Santoyo Noeggerath <68756217+Alannn-dotcom@users.noreply.github.com>
2020-07-31 20:36:17 -0500

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,26 @@
+#"IF ELSE/ELIF CON EDAD"
+#print("Verificacion de acceso")
+#edad_usuario=int(input("Introduce tu edad por favor: "))
+#if edad_usuario<18: 
+    #print("-->No puedes pasar<--")
+#elif edad_usuario>130:
+    #print("fuckyou pinche gringo")
+#else:
+    #print("-->Puedes pasar<--")
+
+
+#"if else/elif con notas"
+print("Nota de alumnos")
+
+nota_alumnos=int(input("Introduzca su nota por favor: "))
+
+if nota_alumnos<5:
+    print("Reprobado")
+elif nota_alumnos<6:
+    print("Suficiente")
+elif nota_alumnos<7:
+    print("tdbn")
+elif nota_alumnos<9:
+    print ("EAAA")
+else:
+    print("Muy bien")
